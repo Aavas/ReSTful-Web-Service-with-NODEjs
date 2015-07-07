36bcffcdb5090b6986e6f165a7e24a542209f05f
Aavas <aavas898@gmail.com>
2015-07-08 02:44:30 +0545

    Fix for naming convention for collections.

--- app.js
+++ app.js
@@ -1,26 +1,22 @@
 var express = require('express'),
     mongoose = require('mongoose');
 
-var db = mongoose.connect('mongodb://localhost:27017/bookAPI', function(err, db){
-    if (err) throw err;
-    else console.log('Successfully connected to the DB' + db);
-});
+var db = mongoose.connect('mongodb://localhost:27017/bookAPI');
 
 var Book = require('./models/bookModels');
 var app = express();
 
 var port = process.env.PORT || 3000;
 
-
 var bookRouter = express.Router();
 
-bookRouter.route('/Books')
+bookRouter.route('/books')
     .get(function(req,res){
-        Book.find(function(err, books){
+        Book.find(function(err, data){
             if(err)
                 console.log(err);
             else
-                res.json(books);
+                res.json(data);
         })
     });
 
--- models/bookModels.js
+++ models/bookModels.js
@@ -8,6 +8,9 @@ var bookModel = new Schema({
     author: {type: String},
     genre: {type: String},
     read: {type: Boolean, default: false}
-});
+}, {collection: 'Book'});   // Mongoose by default produces a collection name by passing the model name to
+                            // the utils.toCollectionName method. This method pluralizes the name. Define collection
+                            // to use different collection. Here 'Book' will be used as collection else 'books' if no collection is defined in schema.
 
-module.exports = mongoose.model('Book' , bookModel);
+module.exports = mongoose.model('Book' , bookModel /*,'Notebook'*/);    // collection name can also be passed as a third argument
+                                                                        // Here, Notebook will be used as collection if uncommented.
